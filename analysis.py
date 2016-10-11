e570518ac96b1be653aace2810513341cf503293
Ami <atsuch@gmail.com>
2016-10-11 01:30:28 -0400

    restructure analysis.py

--- analysis.py
+++ analysis.py
@@ -18,6 +18,7 @@ Do that with a hard loop on the # of components, then
 plotting the mean SAS change.
 """
 
+import os
 import os.path as op
 
 import matplotlib.pyplot as plt
@@ -55,81 +56,127 @@ def getHemiSparsity(img, hemisphere, threshold=0.000005,
     return (pos_arr, neg_arr, abs_arr)
 
 
-def analyzeWBimages(n_component, dataset, scoring,
-                    memory=Memory(cachedir='nilearn_cache'), **kwargs):
+def load_or_generate_summary(images, term_scores, n_components, scoring, dataset,
+                    force=False, memory=Memory(cachedir='nilearn_cache')):
     """
-    For each wb component images, get the # of voxels above an arbitrary threshold
-    (currently hard-coded as 5e-6) to calculate HPI and SAS and return summary DF.
+    For a given n_components, load summary csvs if they already exist, or
+    run main.py to get and save necessary summary data required for plotting.
 
-    HPI(hemisphere participation index): (R-L)/(R+L) for # of voxels above the threshold
-    SAS(spatial asymmetry score): dissimilarity score using the choice of scoring (l1,l2, corr)
-
-    Assumes the component image is already computed and saved.
+    Returns (wb_summary, R_sparsity, L_sparsity), each of which are DataFrame.
     """
-    out_dir = op.join('analysis', dataset)
-    THRESH = 0.000005
+    # Directory to find or save the summary csvs
+    out_dir = op.join('ica_imgs', dataset, 'analyses', str(n_components))
+    summary_csvs = ["wb_summary.csv", "R_sparsity.csv", "L_sparsity.csv"]
 
-    # Get ICA images from ica_nii dir--assumes it exists already
-    print("Simply loading component images for n_component = %s" % c)
-    nii_dir = op.join('ica_nii', dataset, str(n_component))
-    img_path = op.join(nii_dir, 'wb_ica_components.nii.gz')
-    wb_img = NiftiImageWithTerms.from_filename(img_path)
+    # If summary data are already saved as csv files, simply load them
+    if not force and all([op.exists(op.join(out_dir, csv)) for csv in summary_csvs]):
+        print("Loading summary data from %s" % out_dir)
+        (wb_summary, R_sparsity, L_sparsity) = (pd.read_csv(op.join(out_dir, csv))
+                                                for csv in summary_csvs)
 
-    # Store HPI and SAS vals in a DF
-    summary = pd.DataFrame({"n_comp":[n_component]*n_component})
+    # Otherwise run main.py and save them as csv files
+    else:
+        # Initialize summary DFs
+        (wb_summary, R_sparsity, L_sparsity) = (pd.DataFrame(
+                                           {"n_comp":[n_components]*n_components}
+                                           ) for i in range(3))
+        if not op.exists(out_dir):
+            os.makedirs(out_dir)
 
-    # 1) Get # of voxels above the given participation threshold for
-    # pos and neg side, and both (abs), for each hemisphere. Use them
-    # to calculate HPI: (R-L)/(R+L) for # of voxels.
-    hemis = ("R", "L")
-    sparsitySigns = ("pos", "neg", "abs")
-    sparsityResults = {hemi: getHemiSparsity(wb_img, hemi, memory=memory)
-                       for hemi in hemis} # {hemi: (pos_arr, neg_arr, abs_arr)}
-    for i, sign in enumerate(sparsitySigns):
-        for hemi in hemis:
-            summary["%s_%s" % (sign, hemi)] = sparsityResults[hemi][i]
-        summary["%sTotal" % sign] = summary["%s_R" % sign]+summary["%s_L" % sign]
-        summary["%sHPI" % sign] = ((summary["%s_R" % sign]-summary["%s_L" % sign])
-                                  /summary["%sTotal" % sign].astype(float))
+        # Use wb matching in main analysis to get component images and
+        # matching scores
+        match_method = 'wb'
+        img_d, score_mats_d, sign_mats_d = do_main_analysis(
+                    dataset=dataset, images=images, term_scores=term_scores,
+                    key=match_method, force=force, force_match=False,
+                    n_components=n_components, scoring=scoring, plot=False)
 
-    # 2) Get SAS by passing two wb images with hemi labels to compare_aomponents
-    imgs = [wb_img] * 2
-    score_mat, sign_mat = compare_components(imgs, hemis, scoring)
-    # we only care about the diagonal in score_mat (diagonal in sign_mat should all be 1)
-    summary["SAS"] = score_mat.diagonal()
+        # 1) Get sparsity for each hemisphere for "wb", "R" and "L" imgs
+        hemis = ("R", "L")
+        sparsitySigns = ("pos", "neg", "abs")
+        # Dict of DF and labels used to get and store Sparsity results
+        label_dict = {"wb": (wb_summary, hemis),
+                      "R": (R_sparsity, ["R"]),
+                      "L": (L_sparsity, ["L"])}
+        for key in label_dict:
+            (df, labels) = label_dict[key]
+            sparsityResults = {label: getHemiSparsity(img_d[key], label, memory=memory)
+                               for label in labels} # {label: (pos_arr, neg_arr, abs_arr)}
 
-    return summary
+            for i, sign in enumerate(sparsitySigns):
+                for label in labels:
+                    df["%s_%s" % (sign, label)] = sparsityResults[label][i]
+                # For wb only, also compute Total sparsity and HPI
+                if key == "wb":
+                    df["%sTotal" % sign] = df["%s_R" % sign]+df["%s_L" % sign]
+                    df["%sHPI" % sign] = ((df["%s_R" % sign]-df["%s_L" % sign])
+                                          /df["%sTotal" % sign].astype(float))
 
-def plotWBanalysis(n_components, dataset, scoring="test", memory=Memory(cachedir='nilearn_cache'),
-                  **kwargs):
+        # Save R/L_sparsity DFs
+        R_sparsity.to_csv(op.join(out_dir, "R_sparsity.csv"))
+        L_sparsity.to_csv(op.join(out_dir, "L_sparsity.csv"))
+
+        # 2) Get SAS of wb component images as well as matched RL images by passing
+        # 2 x wb or RL images and hemi labels to the compare_components (make sure
+        # not to flip when comparing R and L)
+        name_img_pairs = [("wb_SAS", img_d["wb"]),
+                          ("matchedRL_SAS", img_d["RL"])]
+        for (name, img) in name_img_pairs:
+            sas_imgs = [img] * 2
+            score_mat, sign_mat = compare_components(sas_imgs, hemis, scoring,
+                                                     flip=False)
+            # we only care about the diagonal in score_mat
+            wb_summary[name] = score_mat.diagonal()
+
+        # 3) Finally store indices of matched R, L, and RL components, and the
+        # respective match scores against wb
+        comparisons = [('wb', 'R'), ('wb', 'L'), ('wb', 'RL')]
+        for comparison in comparisons:
+            score_mat, sign_mat = score_mats_d[comparison], sign_mats_d[comparison]
+            matched, unmatched = get_match_idx_pair(score_mat, sign_mat)
+            # Component indices for matched R, L , RL are in matched[1].
+            # Multiply it by matched[2], which stores sign flipping info.
+            matched_indices = matched[1] * matched[2]
+            wb_summary["matched%s" % comparison[1]] = matched_indices
+
+            matched_scores = score_mat[matched[0], matched[1]]
+            wb_summary["match%s_score" % comparison[1]] = matched_scores
+
+            # Save wb_summary
+            wb_summary.to_csv(op.join(out_dir, "wb_summary.csv"))
+
+    return (wb_summary, R_sparsity, L_sparsity)
+
+
+def loop_main_and_plot(components, scoring, dataset, query_server=True, force=False,
+                 memory=Memory(cachedir='nilearn_cache'), **kwargs):
     """
-    Use analyzeWBimages to get HPI and SAS values for each component in a given set of
-    n_components and make a series of plots per n_component, and also make summary plots
-    for HPI and SAS acorss the range of n_components given.
+    Loop main.py to plot summaries of WB vs hemi ICA components
     """
-    # Save plots in the analysis folder
     out_dir = op.join('ica_imgs', dataset, 'analyses')
 
-    # Initialie master DF to store summary DF returned from analyzeWBimages per
-    # specific n_component
-    master_df = pd.DataFrame()
+    # Get the data once.
+    images, term_scores = get_dataset(dataset, max_images=200,  # for testing
+                                      query_server=query_server)
 
-    # Loop over n_components
-    for c in n_components:
-        # Save component-specific images in a subfolder
-        comp_outdir = op.join(out_dir, 'component-specific')
-        # Get data from analyzeWBimages and append to master df
-        summary = analyzeWBimages(c, dataset=dataset, scoring=scoring, memory=memory)
-        master_df = master_df.append(summary)
+    for c in components:
+        print("Running analysis with %d components" % c)
+        (wb_summary, R_sparsity, L_sparsity) = load_or_generate_summary(
+                                                images=images, term_scores=term_scores,
+                                                n_components=c, scoring=scoring,
+                                                dataset=dataset, force=force, memory=memory)
 
-        # Now plot:
+        ### Generate component-specific plots ###
+        # Save component-specific images in the component dir
+        comp_outdir = op.join(out_dir, str(c))
+
+        # 1) Relationship between positive and negative HPI in wb components
         hpi_signs = ['pos', 'neg', 'abs']
-        # 1) Relationship between positive and negative HPI
         # set color to be proportional to the symmetry in the sparsity (Pos-Neg/Abs),
         # and set size to be proportional to the total sparsity (Abs)
-        color = (summary['posTotal'] - summary['negTotal'])/summary['absTotal']
-        size = summary['absTotal']/20.0
-        ax = summary.plot.scatter(x='posHPI', y='negHPI', c=color, s=size,
+        color = (wb_summary['posTotal'] - wb_summary['negTotal'])/wb_summary['absTotal']
+        size = wb_summary['absTotal']/20.0
+        ax = wb_summary.plot.scatter(x='posHPI', y='negHPI', c=color, s=size,
                                   xlim=(-1.1,1.1), ylim=(-1.1,1.1),
                                   colormap='Reds', colorbar=True, figsize=(7, 6))
         title = ax.set_title("\n".join(wrap("The relationship between HPI on "
@@ -155,13 +202,13 @@ def plotWBanalysis(n_components, dataset, scoring="test", memory=Memory(cachedir
         out_path = op.join(comp_outdir, "PosNegHPI_%dcomponents.png" % c)
         save_and_close(out_path)
 
-        # 2) Relationship between HPI and SAS
+        # 2) Relationship between HPI and SAS in wb components
         fh, axes = plt.subplots(1, 3, sharey=True, figsize=(18, 6))
         fh.suptitle("The relationship between HPI values and SAS: "
                     "n_components = %d" % c, fontsize=16)
         hpi_sign_colors = {'pos': 'r', 'neg': 'b', 'abs': 'g'}
         for ax, sign in zip(axes, hpi_signs):
-            ax.scatter(summary['%sHPI' % sign], summary['SAS'],
+            ax.scatter(wb_summary['%sHPI' % sign], wb_summary['SAS'],
                        c=hpi_sign_colors[sign], s=totals[sign]/20)
             ax.set_xlabel("%s HPI" % sign)
             ax.set_xlim(-1.1, 1.1)
@@ -174,11 +221,13 @@ def plotWBanalysis(n_components, dataset, scoring="test", memory=Memory(cachedir
             ax.spines['bottom'].set_position(('data', 0))
             plt.setp(ax, xticks=ticks, xticklabels=labels)
         fh.text(0.04, 0.5, "Spatial Asymmetry Score", va='center', rotation='vertical')
+
         out_path = op.join(comp_outdir, "HPIvsSAS_%dcomponents.png" % c)
         save_and_close(out_path)
 
-    # Now plot the HPI and SAS over a range of n_components
-    # 1) HPI-for pos, neg, and abs_005
+    ### Generate plots over a range of specified n_components ###
+
+    # 1) HPI-for pos, neg, and abs_005 in wb components
     fh, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(18, 6))
     fh.suptitle("Hemispheric Participation Index for each component", fontsize=16)
     hpi_styles = {'pos': ['r', 'lightpink', 'above %d' % THRESH],
@@ -202,7 +251,7 @@ def plotWBanalysis(n_components, dataset, scoring="test", memory=Memory(cachedir
     out_path = op.join(out_dir, 'wb_HPI.png')
     save_and_close(out_path, fh=fh)
 
-    # 2) SAS
+    # 2) SAS for wb components
     fh, ax = plt.subplots(1, 1, figsize=(18, 6))
     fh.suptitle("Spatial Asymmetry Score for each component", fontsize=16)
     sas_mean, sas_sd = by_comp.mean()["SAS"], by_comp.std()["SAS"]
@@ -220,90 +269,6 @@ def plotWBanalysis(n_components, dataset, scoring="test", memory=Memory(cachedir
     save_and_close(out_path, fh=fh)
 
 
-def main_ic_loop(components, scoring,
-                 dataset, query_server=True, force=False,
-                 memory=Memory(cachedir='nilearn_cache'), **kwargs):
-    """
-    Loop main.py to plot summaries of WB vs hemi ICA components
-    """
-    raise NotImplementedError
-    ## Below are old codes from ps.py
-    match_methods = ['wb', 'rl', 'lr']
-    out_dir = op.join('ica_imgs', dataset)
-    mean_scores, unmatched = [], []
-
-    # Get the data once.
-    images, term_scores = get_dataset(
-        dataset, query_server=query_server)
-
-    for match in match_methods:
-        print("Plotting results for %s matching method" % match)
-        mean_score_d, num_unmatched_d = {}, {}
-        for force_match in [False, True]:
-            for c in components:
-                print("Running analysis with %d components" % c)
-                img_d, score_mats_d, sign_mats_d = do_main_analysis(
-                    dataset=dataset, images=images, term_scores=term_scores,
-                    key=match, force=force, force_match=force_match,
-                    n_components=c, scoring=scoring, **kwargs)
-
-                # Get mean dissimilarity scores and number of unmatched for each comparisons
-                # in score_mats_d
-                for comp in score_mats_d:
-                    score_mat, sign_mat = score_mats_d[comp], sign_mats_d[comp]
-                    mia, uma = get_match_idx_pair(score_mat, sign_mat)
-                    mean_score = score_mat[[mia[0], mia[1]]].mean()
-                    n_unmatched = uma.shape[1] if uma is not None else 0
-                    # Store values in respective dict
-                    score_label = "%s%s" % (" vs ".join(comp), "-forced" if force_match else "")
-                    um_label = "unmatched %s%s" % (comp[1], "-forced" if force_match else "")
-                    if c == components[0]:
-                        mean_score_d[score_label] = [mean_score]
-                        num_unmatched_d[um_label] = [n_unmatched]
-                    else:
-                        mean_score_d[score_label].append(mean_score)
-                        num_unmatched_d[um_label].append(n_unmatched)
-
-        # Store vals as df
-        ms_df = pd.DataFrame(mean_score_d, index=components)
-        um_df = pd.DataFrame(num_unmatched_d, index=components)
-        mean_scores.append(ms_df)
-        unmatched.append(um_df)
-        # Save combined df
-        combined = pd.concat([ms_df, um_df], axis=1)
-        out = op.join(out_dir, '%s-matching_simscores.csv' % match)
-        combined.to_csv(out)
-
-    # We have all the scores for the matching method; now plot.
-    fh, axes = plt.subplots(1, 3, sharex=True, sharey=True, figsize=(18, 6))
-    fh.suptitle("Average dissimilarity scores for the best-match pairs", fontsize=16)
-    labels = ["wb vs R", "wb vs L", "R vs L", "L vs R", "wb vs RL",
-              "wb vs R-forced", "wb vs L-forced", "R vs L-forced", "L vs R-forced", "wb vs RL-forced",
-              "unmatched R", "unmatched L", "unmatched RL"]
-    styles = ["r-", "b-", "m-", "m-", "g-",
-              "r:", "b:", "m:", "m:", "g:",
-              "r--", "b--", "m--"]
-    for i, ax in enumerate(axes):
-        ax2 = ax.twinx()
-        ms_df, um_df = mean_scores[i], unmatched[i]
-        for label, style in zip(labels, styles):
-            if label in ms_df.columns:
-                ms_df[label].plot(ax=ax, style=style)
-            elif label in um_df.columns:
-                um_df[label].plot(ax=ax2, style=style)
-        ax.legend()
-        ax.set_title("%s-matching" % (match_methods[i]))
-        ax2.set_ylim(ymax=(um_df.values.max() + 9) // 10 * 10)
-        ax2.legend(loc=4)
-        ax2.set_ylabel("# of unmatched R- or L- components")
-    fh.text(0.5, 0.04, "# of components", ha="center")
-    fh.text(0.05, 0.5, "mean %s scores" % scoring, va='center', rotation='vertical')
-    fh.text(0.95, 0.5, "# of unmatched R- or L- components", va='center', rotation=-90)
-
-    out_path = op.join(out_dir, '%s_simscores.png' % scoring)
-    save_and_close(out_path, fh=fh)
-
-
 if __name__ == '__main__':
     import warnings
     from argparse import ArgumentParser
@@ -315,9 +280,6 @@ if __name__ == '__main__':
     # Arg parsing
     hemi_choices = ['R', 'L', 'wb']
     parser = ArgumentParser(description="Really?")
-    # parser.add_argument('key1', nargs='?', default='R', choices=hemi_choices)
-    # parser.add_argument('key2', nargs='?', default='L', choices=hemi_choices)
-    parser.add_argument('--noSimScore', action='store_true', default=False)
     parser.add_argument('--force', action='store_true', default=False)
     parser.add_argument('--offline', action='store_true', default=False)
     parser.add_argument('--components', nargs='?',
@@ -336,13 +298,5 @@ if __name__ == '__main__':
     components = [int(c) for c in args.pop('components').split(',')]
     dataset=args.pop('dataset')
 
-    # If noSimScore, run only image_analyses
-    if args.pop('noSimScore'):
-        plotWBanalysis(n_components=components, dataset=dataset, **args)
-    # Otherwise run loops, followed by the image_analyses
-    else:
-        main_ic_loop(query_server=query_server,
-                 components=components, **args)
-        image_analyses(query_server=query_server,
-                   components=components, **args)
-    plt.show()
+    loop_main_and_plot(components=components, dataset=dataset,
+                       query_server=query_server, **args)
